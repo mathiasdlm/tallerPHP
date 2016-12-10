67112ae1838fd7446c49f03530de7f9dce73309a
Dario <levy.dario@gmail.com>
2016-12-09 22:24:37 -0300

    primer version de api

---
+++ api/modules/v1/controllers/InmuebleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+class InmuebleController extends ActiveController
+{
+    public $modelClass = 'common\models\Inmueble';
+
+    function conFiltros(){
+    	alert(1);
+    }
+}
+
+?>
