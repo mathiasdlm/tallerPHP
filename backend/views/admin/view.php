843926fb7f0d49e890d066daedb39d256e9acc33
msellanes91 <msellanes91>
2016-12-14 15:21:42 -0300

    fix adminView

--- backend/views/admin/view.php
+++ backend/views/admin/view.php
@@ -31,13 +31,13 @@ $this->title = 'Datos Administrador';
         'attributes' => [
             'id',
             'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
+            //'auth_key',
+            //'password_hash',
+            //'password_reset_token',
             'email:email',
             'status',
-            'created_at',
-            'updated_at',
+            //'created_at',
+            //'updated_at',
             'rol',
         ],
     ]) ?>
